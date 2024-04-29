d6e065b47d896c5de61e195283769393bc33cdb3
Rustam <tabarzin@gmail.com>
2024-04-29 21:36:49 +0300

    refactor: make responsive single movie page

--- kino/src/pages/MoviePage/MoviePage.tsx
+++ kino/src/pages/MoviePage/MoviePage.tsx
@@ -1,21 +1,26 @@
-import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Layout, Typography, Row, Col, Divider } from "antd";
+import { useParams, Link } from "react-router-dom";
 import { moviesStore } from "../../stores/MoviesStore";
-import * as React from "react";
-import { Layout, Typography, Divider, Row, Col } from "antd";
 
 const { Header, Content, Footer } = Layout;
 const { Title, Paragraph } = Typography;
 
-const MoviePage: React.FC = () => {
+const MoviePage = () => {
   const { id } = useParams<{ id: string }>();
-  //   const movie = moviesStore.movies.find((movie) => movie.id === parseInt(id));
   const movie = id
-    ? moviesStore.movies.find((movie) => movie.id === parseInt(id))
+    ? moviesStore.movies.find((m) => m.id === parseInt(id))
     : undefined;
 
-  if (!movie) {
-    return <div>Movie not found</div>;
-  }
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsSmallScreen(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!movie) return <div>Movie not found</div>;
 
   const genres = movie.genres.map((genre) => genre.name).join(", ");
   const countries = movie.countries.map((country) => country.name).join(", ");
@@ -29,19 +34,22 @@ const MoviePage: React.FC = () => {
       </Header>
       <Content style={{ padding: "0 50px" }}>
         <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-          <Row gutter={[16, 16]}>
+          <Row gutter={[16, 16]} justify={isSmallScreen ? "center" : "start"}>
             <Col span={24}>
               <Title>{movie.name}</Title>
               <Divider />
             </Col>
-            <Col span={12}>
+            <Col span={isSmallScreen ? 24 : 12}>
               <img
                 src={movie.poster.url}
                 alt={movie.name}
-                style={{ maxWidth: "100%" }}
+                style={{
+                  maxWidth: "100%",
+                  marginBottom: isSmallScreen ? "16px" : "0",
+                }}
               />
             </Col>
-            <Col span={12}>
+            <Col span={isSmallScreen ? 24 : 12}>
               <Paragraph>
                 <strong>Год:</strong> {movie.year}
               </Paragraph>
